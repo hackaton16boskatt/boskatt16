b28512bba892abb091a00f83557c7fe22c70e51c
Reine Ahlgren <reineelias@gmail.com>
2016-09-15 17:00:37 +0200

    A baseplate for Clojure

---
+++ src/boskatt16/main.clj
@@ -0,0 +1,30 @@
+(ns boskatt16.main
+  (:use ring.adapter.jetty
+        compojure.core
+        compojure.route)
+  (:require [compojure.handler :as handler]
+            [net.cgrand.enlive-html :as enlive])
+  (:gen-class))
+
+(def things ["one" "two" "three" "four"])
+
+(defn extract-body [html]
+  (enlive/at html [#{:html :body}] enlive/unwrap))
+
+(enlive/deftemplate layout "layout.html" [title content]
+  [#{:title :h1}] (enlive/content title)
+  [:div.content] (enlive/substitute (extract-body content)))
+
+(defn show [things]
+  (enlive/at (enlive/html-resource "show.html")
+             [:li] (enlive/clone-for [thing things] (enlive/content thing))))
+
+(def index (enlive/html-resource "index.html"))
+
+(defroutes app
+  (GET "/" [] (layout "Front page" index))
+  (GET "/show" [] (layout "Show things" (show things)))
+  (not-found "Not Found"))
+
+(defn -main []
+  (run-jetty (handler/site app) {:port 8080}))
